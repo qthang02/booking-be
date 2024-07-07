f529172f487171e246b5e5238cc2999ac852df87
thangnq <thangng1@vnpay.vn>
2024-07-07 09:15:13 +0700

    add category model

---
+++ enities/category.go
@@ -0,0 +1,16 @@
+package enities
+
+import (
+	"github.com/qthang02/booking/types"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	gorm.Model
+	Name           string             `json:"name"`
+	Description    string             `json:"description"`
+	ImageLink      string             `json:"image_link"`
+	Price          float64            `json:"price"`
+	AvailableRooms uint64             `json:"available_rooms"`
+	Type           types.CategoryType `json:"type"`
+}
---
+++ types/types.go
@@ -0,0 +1,12 @@
+package types
+
+type CategoryType int
+
+const (
+	ORIGINAL CategoryType = iota
+	VIP1
+	VIP2
+	POPULAR
+	SEPARATELY
+	DELUXE
+)
